666fc94dcaa42c7ea7af4543a312ff823a8c3c3d
j-mist12 <morenojames395@gmail.com>
2026-05-02 19:35:23 +0800

    item resource, damage popup update,.
    added 3 items, regen, mult item and flat damage item. damage updates from the ui.

---
+++ Script/ItemResourceBase.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name ItemResource
+
+@export var texture: Texture
+@export var name: String
+@export var desc: String
+@export var mult: float
+@export var FlatDmg: int
+@export var lifesteal: int
+@export var regen: int
+@export var regen_CD: float
--- Script/player.gd
+++ Script/player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
-@export var healthbar: ProgressBar
-var max_hp: int = 10000
-var hp: int = 10000
-
+class_name player
 @onready var game: Node = $".."
+@export var healthbar: ProgressBar
 @onready var axis: Node2D = $Axis
-
+@onready var regen_timer: Timer = $Timer
+var items: Array[ItemResource] = []
+var max_hp: int = 100
+var hp: int = max_hp
+var dmg_mult: float = 1.0
+var flat_dmg: int = 0
+var regen_cooldown:Dictionary= {}
+var lifesteal: float
 func _ready() -> void:
+	regen_timer.timeout.connect(_on_regen_timeout)
+	var item: ItemResource = preload("res://Resource/Items/9for3.tres")
+	get_item(item)
 	hp = max_hp
 	if not healthbar:
 		healthbar = get_tree().root.find_child("Healthbar", true, false)
@@ -17,6 +25,18 @@ func _ready() -> void:
 		print("Healthbar initialized")
 	else:
 		print("Warning: Healthbar not found")
+
+#get item function#
+
+func get_item(item: ItemResource) -> void:
+	items.append(item)
+	print("You got: ", item.name)
+	dmg_mult *= item.mult
+	flat_dmg += item.FlatDmg
+	regen_cooldown[item] = 0.0
+	lifesteal = item.lifesteal
+
+#get closest enemy, for attacking, only returns a location
 
 func closest_enemy() -> Node2D:
 	var enemies: Array = get_tree().get_nodes_in_group("enemies")
@@ -29,16 +49,47 @@ func closest_enemy() -> Node2D:
 			closest = i
 	return closest
 
-func attack(damage: int) -> void:
+# dmg mult for items
+func modify_dmg(base_damage: int ) -> int:
+	return int(base_damage * dmg_mult) + flat_dmg
+	
+func attack(damage: int) -> int:
+	var final_damage= modify_dmg(damage)
 	var enemy: Node2D = closest_enemy()
+	print("Base: ", flat_dmg, "mult", dmg_mult, "final: ", final_damage)
 	if enemy:
 		axis.look_at(enemy.global_position)
 		axis.rotation += PI / 2
-		enemy.take_dmg(damage)
+		enemy.take_dmg(final_damage)
+		for item in items:
+			if item.lifesteal >0:
+				var heal_amount = final_damage * item.lifesteal
+				heal(heal_amount)
+			
 	print("shoot")
+	return final_damage
+
+#for healing
+func heal(amount: float) -> void:
+	if amount <= 0:
+		return
+	hp+= int(amount)
+	hp =clamp(hp,0,max_hp)
+	if healthbar:
+		healthbar.health = hp
+		
+func _on_regen_timeout()-> void:
+	for item in items:
+		if item.regen >0:
+			regen_cooldown[item] += 1.0
+			if regen_cooldown[item]>= item.regen_CD:
+				heal(item.regen)
+				print(hp)
+				regen_cooldown[item] = 0.0
 
 func hurt(amount: int) -> void:
-	hp -= amount
+	var modify_damage = int(amount * dmg_mult)
+	hp -= modify_damage
 	hp = clamp(hp, 0, max_hp)
 	print("you missed lol")
 	if healthbar:
